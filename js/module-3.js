ae5ec7dbabd12065150f4551c3d701997fc02a35
Oleksandr-Smetana <anatemso@gmail.com>
2021-05-29 16:34:12 +0300

    добавляет решения 30 задач модуля 3

--- js/module-3.js
+++ js/module-3.js
@@ -158,3 +158,391 @@
 //   keys.push(key);
 //   values.push(apartment[key]);
 // }
+
+// console.log(keys);
+// console.log(values);
+
+// ========== T11 ========== Метод hasOwnProperty() ==========
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// console.log(apartment.service);
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+//     }
+//   // Change code above this line
+// }
+
+// console.log(keys);
+// console.log(values);
+// console.log(advert);
+// console.log(apartment);
+
+// ========== T12 ========== Задача. Подсчёт свойств ==========
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for (const obj in object) {
+//     if (object.hasOwnProperty(obj))
+//     propCount += 1;
+//   }
+//   // Change code above this line
+//   return propCount;
+// };
+
+// console.log(countProps({}));
+// console.log(countProps({ name: "Mango", age: 2 }));
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+// ========== T13 ========== Метод Object.keys() ==========
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(values);
+
+// ========== T14 ========== Задача. Подсчёт свойств 2.0 ==========
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+
+//   for (const key of keys) {
+//       propCount += 1;
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+// console.log(countProps({}));
+// console.log(countProps({ name: "Mango", age: 2 }));
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+// ========== T15 ========== Метод Object.values() ==========
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// ========== T16 ========== Задача. Расходы на зарплату ==========
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   for (const value of values) {
+//     totalSalary += value;
+//   }
+//   // Change code above this line
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({}));
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// ========== T17 ========== Цикл for...in ==========
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+//  for (const color of colors) {
+//    hexColors.push(color.hex);
+//    rgbColors.push(color.rgb);
+// }
+ 
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// ========== T18 ========== Задача. Поиск объекта по значению свойства ==========
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       return product.price;
+//     };
+//   };
+//   return null;
+//   // Change code above this line
+// }
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+
+// ========== T19 ========== Задача. Коллекция значений свойства ==========
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const values = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       values.push(product[propName]);
+//     };
+//   };
+//   return values;
+//   // Change code above this line
+// }
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
+
+// ========== T20 ========== Задача. Общая стоимость товара ==========
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let sum = 0;
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       sum = product.price * product.quantity;
+//     }
+//   }
+//   return sum;
+//   // Пиши код выше этой строки
+// }
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
+
+// ========== T21 ========== Деструктуризация объектов ==========
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const { yesterday, today, tomorrow, } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(meanTemperature);
+
+// ========== T22 ========== Значения по умолчанию ==========
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const {
+//     yesterday,
+//     today,
+//     tomorrow,
+//     icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"
+// } = highTemperatures;
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(meanTemperature);
+// console.log(icon);
+
+// ========== T23 ========== Изменение имени переменной ==========
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const { 
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// ========== T24 ========== Деструктуризация в циклах ==========
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// // for (const color of colors) {
+// //     const { hex, rgb, } = color;
+// //   hexColors.push(hex);
+// //   rgbColors.push(rgb);
+// // }
+
+// for (const { hex, rgb, } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// ========== T25 ========== Глубокая деструктуризация ==========
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+// const { 
+//   today: { 
+//     low: lowToday, 
+//     high: highToday,
+//     icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   },
+//   tomorrow: { 
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//    },
+// } = forecast;
+
+// ========== T26 ========== Паттерн «Объект настроек» ==========
+// Change code below this line
+// function calculateMeanTemperature(forecast) {
+//   const { 
+//   today: { low: todayLow, high: todayHigh },
+//   tomorrow: { low: tomorrowLow, high: tomorrowHigh } 
+// } = forecast;
+
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+// console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }));
+// console.log(calculateMeanTemperature({ today: {low: 37, high: 40}, tomorrow: {low: 33, high: 38} }));
+
+// ========== T27 ========== Операция spread при передаче аргументов ==========
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+// console.log(bestScore);
+// console.log(worstScore);
+
+// ========== T28 ========== Операция spread при создании нового массива ==========
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
+// console.log(allScores);
+// console.log(bestScore);
+// console.log(worstScore);
+
+// ========== T29 ========== Операция spread при создании нового объекта ==========
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings};
+
+// console.log(finalSettings);
+
+// ========== T30 ========== Задача. Карточки задач ==========
+function makeTask(data) {
+  const completed = false;
+  const category = 'General';
+  const priority = 'Normal';
+  // Change code below this line
+  const newData = { completed, category, priority, ...data };
+  return newData;
+  // Change code above this line
+}
+console.log(makeTask({}));
+console.log(makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" }));
+console.log(makeTask({ category: "Finance", text: "Take interest" }));
+console.log(makeTask({ priority: "Low", text: "Choose shampoo" }));
+console.log(makeTask({ text: "Buy bread" }));
+
+// ========== T31 ========== Задача. Карточки задач ==========
+
+// ========== T32 ========== Задача. Карточки задач ==========
+
+// ========== T33 ========== Задача. Карточки задач ==========
+
+// ========== T34 ========== Задача. Карточки задач ==========
+
+// ========== T35 ========== Задача. Карточки задач ==========
+
+// ========== T36 ========== Задача. Карточки задач ==========
+
+// ========== T37 ========== Задача. Карточки задач ==========
+
+// ========== T38 ========== Задача. Карточки задач ==========
+
+// ========== T39 ========== Задача. Карточки задач ==========
+
+// ========== T40 ========== Задача. Карточки задач ==========
+
+// ========== T41 ========== Задача. Карточки задач ==========
