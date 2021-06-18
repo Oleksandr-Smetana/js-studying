3a2b2ca57cf1cb7aca6810cfd80b5d087366df7d
Oleksandr-Smetana <anatemso@gmail.com>
2021-06-18 20:05:54 +0300

    добавляет решение 10и задач модуля 6

--- js/module-6.js
+++ js/module-6.js
@@ -8,8 +8,8 @@
 //   orderedItems.forEach(function(number, i) {
 //     totalPrice += orderedItems[i];
 //   });
-  
-  
+
+
 
 //   // Пиши код выше этой строки
 //   return totalPrice;
@@ -23,17 +23,17 @@
 // function filterArray(numbers, value) {
 //     const filteredNumbers = [];
 //     // Пиши код ниже этой строки
-  
+
 //     numbers.forEach(function(number) {
 //       if (number > value) {
 //         filteredNumbers.push(number);
 //       }
 //     });
-  
+
 //     // Пиши код выше этой строки
 //     return filteredNumbers;
 // }
-  
+
 // console.log(filterArray([1, 2, 3, 4, 5], 3));
 // console.log(filterArray([1, 2, 3, 4, 5], 4));
 // console.log(filterArray([1, 2, 3, 4, 5], 5));
@@ -44,7 +44,7 @@
 // function getCommonElements(firstArray, secondArray) {
 //     const commonElements = [];
 //     // Пиши код ниже этой строки
-    
+
 //     firstArray.forEach(function(element, i) {
 //        if (secondArray.includes(element)) {
 //          commonElements.push(element);
@@ -62,23 +62,99 @@
 
 // ========== T4 ========== Стрелочные функции ==========
 // Пиши код ниже этой строки
-const calculateTotalPrice = (quantity, pricePerItem) => {
-  // Пиши код выше этой строки
-  return quantity * pricePerItem;
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   // Пиши код выше этой строки
+//   return quantity * pricePerItem;
+// }
+
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+
+// ========== T5 ========== Неявный возврат ==========
+// // Пиши код ниже этой строки
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// // Пиши код выше этой строки
+
+// ========== T6 ========== Стрелочные функции как коллбеки ==========
+// // Пиши код ниже этой строки
+// const calculateTotalPrice = orderedItems => {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(item => totalPrice += item);
+
+//   return totalPrice;
+// }
+// // Пиши код выше этой строки
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// ========== T7 ========== Задача. Фильтрация массива чисел 2.0 ==========
+// // Пиши код ниже этой строки
+// const filterArray = (numbers, value) => {
+//     const filteredNumbers = [];
+  
+//     numbers.forEach(number => {
+//       if (number > value) {
+//         filteredNumbers.push(number);
+//       }
+//     });
+  
+//     // Пиши код выше этой строки
+//     return filteredNumbers;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// ========== T8 ========== Задача. Общие элементы 2.0 ==========
+// // Пиши код ниже этой строки
+// const getCommonElements = (firstArray, secondArray) => {
+//     const commonElements = [];
+  
+//     firstArray.forEach(element => {
+//       if (secondArray.includes(element)) {
+//         commonElements.push(element);
+//       }
+//     });
+// // Пиши код выше этой строки
+//     return commonElements;
+// }
+  
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+// ========== T9 ========== Чистые функции ==========
+function changeEven(numbers, value) {
+    // Пиши код ниже этой строки
+  let newNumbers = [];
+  numbers.forEach((number) => {
+    if (number % 2 === 0) {
+      newNumbers.push(number + value);
+    } else {
+      newNumbers.push(number);
+    }
+  });
+  return newNumbers;
+    // Пиши код выше этой строки
 }
 
-console.log(calculateTotalPrice(5, 100));
-console.log(calculateTotalPrice(8, 60));
-console.log(calculateTotalPrice(3, 400));
+console.log(changeEven([1, 2, 3, 4, 5], 10));
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+console.log(changeEven([17, 24, 68, 31, 42], 100));
+console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
 
-// ========== T5 ========== 00 ==========
+// ========== T10 ========== Метод map() ==========
+const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+// Пиши код ниже этой строки
+const planetsLengths = planets.map(planet => planet.length);
 
-// ========== T6 ========== 00 ==========
-
-// ========== T7 ========== 00 ==========
-
-// ========== T8 ========== 00 ==========
-
-// ========== T9 ========== 00 ==========
-
-// ========== T10 ========== 00 ==========
+console.log(planetsLengths);
